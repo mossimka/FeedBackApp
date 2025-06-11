455556d8a9dc4ed34add7d723969246711e85d1e
mossimka <maksimsarsekeev@gmail.com>
2025-06-11 17:20:20 +0500

    unnecessary

--- back/src/main.py
+++ back/src/main.py
@@ -20,7 +20,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://207.154.221.138:5173",
-    "http://feedbackapp-frontend-1:5173"
+    "http://feedbackapp-frontend-1:5173",
+    "http://207.154.221.138:5173/"
 ]
 
 app.add_middleware(
